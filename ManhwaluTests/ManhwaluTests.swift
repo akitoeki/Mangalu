e28f2636a04b54bd9124a728f33ddeb084261638
Nhan Dang <nhan@missingcorner.com>
2020-09-16 11:43:16 +0700

    Initial Commit

---
+++ ManhwaluTests/ManhwaluTests.swift
@@ -0,0 +1,34 @@
+//
+//  ManhwaluTests.swift
+//  ManhwaluTests
+//
+//  Created by Nhan Dang on 9/16/20.
+//  Copyright © 2020 Nhan Dang. All rights reserved.
+//
+
+import XCTest
+@testable import Manhwalu
+
+class ManhwaluTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
